8be9b08fc3fc88caff0b008292484f03d8adda16
hafid29 <hafidwibisana29@gmail.com>
2022-03-24 14:18:44 +0700

    pembaruan

--- aplas/tests/Unit/ExampleTest.php
+++ aplas/tests/Unit/ExampleTest.php
@@ -15,4 +15,30 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    /**
+     * Testing query create database with wrongSyntax (kreate)
+     * @return void
+     */
+    public function testCreateDatabaseWithKreateSytax()
+    {
+        // siswa melakukan query untuk membuat database
+        // query yg di masukan siswa adalah "KREATE DATABASE 'name'"
+        // sedangkan ekspetasi untuk query tersebut adalah "CREATE DATABASE"
+        // maka dihalaman pembelajaran akan muncul alert 'query salah'
+    }
+    
+    /**
+     * Testing query create database with correct syntax (CREATE DATABASE)
+     * @return void
+     */
+    public function testCreateDatabaseWithCorrectSyntax() {
+        // siswa melakukan query untuk create database
+        // qwuery yg dimasukan adalah "CREATE DATABASE 'name'"
+        // sedangkan ekspetasi nya adalah "CREATE DATABASE 'name'"
+        // maka dihalaman pembelajan akan muncul pesan "query betul dan akan menampilkan hasil"
+    }
+    
+    /**
+     * 
+     */
 }
